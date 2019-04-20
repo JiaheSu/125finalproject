8b5b7c1f2740c3368bb8b220483e1725fd16f53e
JiaheSu <jiahesu2@illinois.edu>
2019-04-20 01:13:31 -0500

    start

--- app/src/main/java/com/example/a125finalproject/IntroActivity.java
+++ app/src/main/java/com/example/a125finalproject/IntroActivity.java
@@ -2,12 +2,25 @@ package com.example.a125finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class IntroActivity extends AppCompatActivity {
+    private ImageButton imageButtonTL;
+    private ImageButton imageButtonTR;
+    private ImageButton imageButtonDL;
+    private ImageButton imageButtonDR;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        imageButtonTL = findViewById(R.id.imageButtonTL);
+
+        imageButtonTR = findViewById(R.id.imageButtonTR);
+
+        imageButtonDL = findViewById(R.id.imageButtonDL);
+
+        imageButtonDR = findViewById(R.id.imageButtonDR);
     }
 }
