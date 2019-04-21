e18b716ade587eecaf66a5b833373391a9c095ac
JiaheSu <jiahesu2@illinois.edu>
2019-04-20 19:09:58 -0500

    start

--- app/src/main/java/com/example/a125finalproject/TaskActivity.java
+++ app/src/main/java/com/example/a125finalproject/TaskActivity.java
@@ -1,13 +1,29 @@
 package com.example.a125finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TaskActivity extends AppCompatActivity {
+    private Button buttonCamera;
+    private Button buttonFinish;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_task);
+
+        buttonCamera = findViewById(R.id.buttonCamera);
+
+        buttonFinish = findViewById(R.id.buttonFinish);
+        buttonFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TaskActivity.this, AlbumActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
