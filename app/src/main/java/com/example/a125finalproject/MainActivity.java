244556fcc0a9b7f83f2030cdab5b5ebf4a605c94
JiaheSu <jiahesu2@illinois.edu>
2019-04-23 22:36:36 -0500

    start

--- app/src/main/java/com/example/a125finalproject/MainActivity.java
+++ app/src/main/java/com/example/a125finalproject/MainActivity.java
@@ -5,9 +5,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView imageView;
     private Button buttonStart;
     private Button buttonAlbum;
     private TextView textViewQuote;
@@ -16,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
 
         buttonStart = findViewById(R.id.buttonStart);
         buttonStart.setOnClickListener(new View.OnClickListener() {
