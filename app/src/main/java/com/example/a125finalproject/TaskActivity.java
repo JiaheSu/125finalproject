5699c68a08e62305f7f50fcf8abc3d863b6bcd39
JiaheSu <jiahesu2@illinois.edu>
2019-04-29 17:55:23 -0500

    pppp

--- app/src/main/java/com/example/a125finalproject/TaskActivity.java
+++ app/src/main/java/com/example/a125finalproject/TaskActivity.java
@@ -1,5 +1,6 @@
 package com.example.a125finalproject;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+
+import java.io.FileOutputStream;
 
 public class TaskActivity extends AppCompatActivity {
     private ImageButton imageBToL;
@@ -75,12 +78,41 @@ public class TaskActivity extends AppCompatActivity {
         buttonFinish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent2 = new Intent(TaskActivity.this, AlbumActivity.class);
-                intent2.putExtra("1stPic", bmp1);
-                intent2.putExtra("2ndPic", bmp2);
-                intent2.putExtra("3rdPic", bmp3);
-                intent2.putExtra("4thPic", bmp4);
-                startActivity(intent2);
+                try {
+                    //Write file
+                    String filename1 = "bitmap1.png";
+                    FileOutputStream stream1 = getApplicationContext().openFileOutput(filename1, Context.MODE_PRIVATE);
+                    bmp1.compress(Bitmap.CompressFormat.PNG, 100, stream1);
+                    String filename2 = "bitmap2.png";
+                    FileOutputStream stream2 = getApplicationContext().openFileOutput(filename2, Context.MODE_PRIVATE);
+                    bmp2.compress(Bitmap.CompressFormat.PNG, 100, stream2);
+                    String filename3 = "bitmap3.png";
+                    FileOutputStream stream3 = getApplicationContext().openFileOutput(filename3, Context.MODE_PRIVATE);
+                    bmp3.compress(Bitmap.CompressFormat.PNG, 100, stream3);
+                    String filename4 = "bitmap4.png";
+                    FileOutputStream stream4 = getApplicationContext().openFileOutput(filename4, Context.MODE_PRIVATE);
+                    bmp4.compress(Bitmap.CompressFormat.PNG, 100, stream4);
+
+                    //Cleanup
+                    stream1.close();
+                    bmp1.recycle();
+                    stream2.close();
+                    bmp2.recycle();
+                    stream3.close();
+                    bmp3.recycle();
+                    stream4.close();
+                    bmp4.recycle();
+
+                    //Pop intent
+                    Intent in1 = new Intent(TaskActivity.this, AlbumActivity.class);
+                    in1.putExtra("image1", filename1);
+                    in1.putExtra("image2", filename2);
+                    in1.putExtra("image3", filename3);
+                    in1.putExtra("image4", filename4);
+                    startActivity(in1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
             }
         });
     }
