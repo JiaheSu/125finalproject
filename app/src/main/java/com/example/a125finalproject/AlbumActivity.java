9b312d6b26bcba13d900b0cedaee0339e5f6be63
JiaheSu <jiahesu2@illinois.edu>
2019-04-29 18:01:47 -0500

    pppp

--- app/src/main/java/com/example/a125finalproject/AlbumActivity.java
+++ app/src/main/java/com/example/a125finalproject/AlbumActivity.java
@@ -2,9 +2,12 @@ package com.example.a125finalproject;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
+
+import java.io.FileInputStream;
 
 public class AlbumActivity extends AppCompatActivity {
     private ImageView imageView1st;
@@ -17,35 +20,43 @@ public class AlbumActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album);
 
-        Intent intent1 = getIntent();
-        Bitmap bit1 = intent1.getParcelableExtra("1stPic");
+        String filename1 = getIntent().getStringExtra("image1");
         try {
-            imageView1st.setImageBitmap(bit1);
-        } catch (NullPointerException e) {
+            FileInputStream is = this.openFileInput(filename1);
+            Bitmap bmp1 = BitmapFactory.decodeStream(is);
+            imageView1st.setImageBitmap(bmp1);
+            is.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        Intent intent2 = getIntent();
-        Bitmap bit2 = intent2.getParcelableExtra("2ndPic");
+        String filename2 = getIntent().getStringExtra("image2");
         try {
-            imageView2nd.setImageBitmap(bit2);
-        } catch (NullPointerException e) {
+            FileInputStream is = this.openFileInput(filename2);
+            Bitmap bmp2 = BitmapFactory.decodeStream(is);
+            imageView2nd.setImageBitmap(bmp2);
+            is.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        Intent intent3 = getIntent();
-        Bitmap bit3 = intent3.getParcelableExtra("3rdPic");
+        String filename3 = getIntent().getStringExtra("image3");
         try {
-            imageView3rd.setImageBitmap(bit3);
-        } catch (NullPointerException e) {
+            FileInputStream is = this.openFileInput(filename3);
+            Bitmap bmp3 = BitmapFactory.decodeStream(is);
+            imageView3rd.setImageBitmap(bmp3);
+            is.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        Intent intent4 = getIntent();
-        Bitmap bit4 = intent4.getParcelableExtra("4thPic");
+        String filename4 = getIntent().getStringExtra("image4");
         try {
-            imageView4th.setImageBitmap(bit4);
-        } catch (NullPointerException e) {
+            FileInputStream is = this.openFileInput(filename4);
+            Bitmap bmp4 = BitmapFactory.decodeStream(is);
+            imageView4th.setImageBitmap(bmp4);
+            is.close();
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
