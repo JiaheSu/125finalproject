24918f34f5c7998f261b3da0a1f4973670e9ff9c
JiaheSu <jiahesu2@illinois.edu>
2019-04-28 19:02:34 -0500

    Merge branch 'master' of https://github.com/JiaheSu/125finalproject
    # Conflicts:
    #	app/src/main/java/SupportFeatures/Azure.java

--- app/src/main/java/com/example/a125finalproject/TaskActivity.java
+++ app/src/main/java/com/example/a125finalproject/TaskActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.util.List;
+
 public class TaskActivity extends AppCompatActivity {
     private Button buttonCamera;
     private Button buttonFinish;
@@ -16,7 +18,7 @@ public class TaskActivity extends AppCompatActivity {
     private ImageView imageViewToR;
     private ImageView imageViewDoL;
     private ImageView imageViewDoR;
-    private Intent intent;
+    private List<Intent> list;
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +29,9 @@ public class TaskActivity extends AppCompatActivity {
         buttonCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 if (intent.resolveActivity(getPackageManager()) != null) {
+                    list.add(intent);
                     startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
                 }
             }
@@ -44,18 +47,30 @@ public class TaskActivity extends AppCompatActivity {
         });
 
         imageViewToL = findViewById(R.id.imageViewToL);
-
         imageViewToR = findViewById(R.id.imageViewToR);
-
         imageViewDoL = findViewById(R.id.imageViewDoL);
-
         imageViewDoR = findViewById(R.id.imageViewDoR);
     }
+
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = intent.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageViewToL.setImageBitmap(imageBitmap);
+            Intent intent1 = list.get(0);
+            Intent intent2 = list.get(1);
+            Intent intent3 = list.get(2);
+            Intent intent4 = list.get(3);
+
+            Bundle extras1 = intent1.getExtras();
+            Bitmap imageBitmap1 = (Bitmap) extras1.get("data");
+            imageViewToL.setImageBitmap(imageBitmap1);
+            Bundle extras2 = intent2.getExtras();
+            Bitmap imageBitmap2 = (Bitmap) extras2.get("data");
+            imageViewToR.setImageBitmap(imageBitmap2);
+            Bundle extras3 = intent3.getExtras();
+            Bitmap imageBitmap3 = (Bitmap) extras3.get("data");
+            imageViewDoL.setImageBitmap(imageBitmap3);
+            Bundle extras4 = intent4.getExtras();
+            Bitmap imageBitmap4 = (Bitmap) extras4.get("data");
+            imageViewDoR.setImageBitmap(imageBitmap4);
         }
     }
 }
