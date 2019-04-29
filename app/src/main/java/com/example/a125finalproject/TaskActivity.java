4ae4357041c984f87c843bf7e94d2ac19d3c8dd1
JiaheSu <jiahesu2@illinois.edu>
2019-04-29 02:16:30 -0500

    pppp

--- app/src/main/java/com/example/a125finalproject/TaskActivity.java
+++ app/src/main/java/com/example/a125finalproject/TaskActivity.java
@@ -2,20 +2,11 @@ package com.example.a125finalproject;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 public class TaskActivity extends AppCompatActivity {
     private Button buttonCamera;
