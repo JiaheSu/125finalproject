0f23060ee9d8c6c35f20256a7310985682365c2a
JiaheSu <jiahesu2@illinois.edu>
2019-04-29 01:39:17 -0500

    Merge branch 'master' of https://github.com/JiaheSu/125finalproject
    # Conflicts:
    #	app/src/main/java/SupportFeatures/Azure.java

--- app/src/main/java/com/example/a125finalproject/TaskActivity.java
+++ app/src/main/java/com/example/a125finalproject/TaskActivity.java
@@ -3,13 +3,17 @@ package com.example.a125finalproject;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +28,10 @@ public class TaskActivity extends AppCompatActivity {
     private Intent intentII;
     private Intent intentIII;
     private Intent intentIV;
+    private Uri imageUriI;
+    private Uri imageUriII;
+    private Uri imageUriIII;
+    private Uri imageUriIV;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,19 +42,87 @@ public class TaskActivity extends AppCompatActivity {
         buttonCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                File outputI = new File(getExternalCacheDir(), "output_I.jpg");
+                try {
+                    if (outputI.exists()) {
+                        outputI.delete();
+                    }
+                    outputI.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= 24) {
+                    imageUriI = FileProvider.getUriForFile(TaskActivity.this,
+                            "com.example.cameraalbum.fileprovider", outputI);
+                } else {
+                    imageUriI = Uri.fromFile(outputI);
+                }
+
+                File outputII = new File(getExternalCacheDir(), "output_II.jpg");
+                try {
+                    if (outputII.exists()) {
+                        outputII.delete();
+                    }
+                    outputII.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= 24) {
+                    imageUriII = FileProvider.getUriForFile(TaskActivity.this,
+                            "com.example.cameraalbum.fileprovider", outputII);
+                } else {
+                    imageUriII = Uri.fromFile(outputII);
+                }
+
+                File outputIII = new File(getExternalCacheDir(), "output_III.jpg");
+                try {
+                    if (outputIII.exists()) {
+                        outputIII.delete();
+                    }
+                    outputIII.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= 24) {
+                    imageUriIII = FileProvider.getUriForFile(TaskActivity.this,
+                            "com.example.cameraalbum.fileprovider", outputIII);
+                } else {
+                    imageUriIII = Uri.fromFile(outputIII);
+                }
+
+                File outputIV = new File(getExternalCacheDir(), "output_IV.jpg");
+                try {
+                    if (outputIV.exists()) {
+                        outputIV.delete();
+                    }
+                    outputIV.createNewFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                if (Build.VERSION.SDK_INT >= 24) {
+                    imageUriIV = FileProvider.getUriForFile(TaskActivity.this,
+                            "com.example.cameraalbum.fileprovider", outputIV);
+                } else {
+                    imageUriIV = Uri.fromFile(outputIV);
+                }
+
                 intentI = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                intentI.putExtra(MediaStore.EXTRA_OUTPUT, imageUriI);
                 if (intentI.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(intentI, 1);
                 }
                 intentII = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                intentII.putExtra(MediaStore.EXTRA_OUTPUT, imageUriII);
                 if (intentII.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(intentII, 2);
                 }
                 intentIII = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                intentIII.putExtra(MediaStore.EXTRA_OUTPUT, imageUriIII);
                 if (intentIII.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(intentIII, 3);
                 }
                 intentIV = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                intentIV.putExtra(MediaStore.EXTRA_OUTPUT, imageUriIV);
                 if (intentIV.resolveActivity(getPackageManager()) != null) {
                     startActivityForResult(intentIV, 4);
                 }
