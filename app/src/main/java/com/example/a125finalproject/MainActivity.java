1725a6f6913e4754f9d292e855e5c0c3f555d5c8
JiaheSu <jiahesu2@illinois.edu>
2019-04-29 16:06:08 -0500

    pppp

--- app/src/main/java/com/example/a125finalproject/MainActivity.java
+++ app/src/main/java/com/example/a125finalproject/MainActivity.java
@@ -9,16 +9,12 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import android.app.DownloadManager;
-
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.gson.JsonObject;
-
 
 import org.json.JSONArray;
 import org.json.JSONException;
