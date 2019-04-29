be57b73452de68c0c5ac9cdb0b139d1780eb6421
JiaheSu <jiahesu2@illinois.edu>
2019-04-28 19:59:31 -0500

    Merge branch 'master' of https://github.com/JiaheSu/125finalproject
    # Conflicts:
    #	app/src/main/java/SupportFeatures/Azure.java

--- app/src/main/java/com/example/a125finalproject/TaskActivity.java
+++ app/src/main/java/com/example/a125finalproject/TaskActivity.java
@@ -18,7 +18,6 @@ public class TaskActivity extends AppCompatActivity {
     private ImageView imageViewToR;
     private ImageView imageViewDoL;
     private ImageView imageViewDoR;
-    private List<Intent> list;
     private Intent intentI;
     static final int REQUEST_IMAGE_CAPTURE = 1;
 
@@ -36,7 +35,6 @@ public class TaskActivity extends AppCompatActivity {
                 }
             }
         });
-        list.add(intentI);
 
         buttonFinish = findViewById(R.id.buttonFinish);
         buttonFinish.setOnClickListener(new View.OnClickListener() {
@@ -48,30 +46,13 @@ public class TaskActivity extends AppCompatActivity {
         });
 
         imageViewToL = findViewById(R.id.imageViewToL);
-        imageViewToR = findViewById(R.id.imageViewToR);
-        imageViewDoL = findViewById(R.id.imageViewDoL);
-        imageViewDoR = findViewById(R.id.imageViewDoR);
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
         if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Intent intent1 = list.get(0);
-            Intent intent2 = list.get(1);
-            Intent intent3 = list.get(2);
-            Intent intent4 = list.get(3);
-
-            Bundle extras1 = intent1.getExtras();
+            Bundle extras1 = intentI.getExtras();
             Bitmap imageBitmap1 = (Bitmap) extras1.get("data");
             imageViewToL.setImageBitmap(imageBitmap1);
-            Bundle extras2 = intent2.getExtras();
-            Bitmap imageBitmap2 = (Bitmap) extras2.get("data");
-            imageViewToR.setImageBitmap(imageBitmap2);
-            Bundle extras3 = intent3.getExtras();
-            Bitmap imageBitmap3 = (Bitmap) extras3.get("data");
-            imageViewDoL.setImageBitmap(imageBitmap3);
-            Bundle extras4 = intent4.getExtras();
-            Bitmap imageBitmap4 = (Bitmap) extras4.get("data");
-            imageViewDoR.setImageBitmap(imageBitmap4);
         }
     }
 }
